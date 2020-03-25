684d6a810b7e9e4225a096cc444efc53072b44c3
Michael Whelan <mwhelanj@tcd.ie>
2020-03-25 15:54:20 +1100

    read from embedded resource and print each hand

--- PokerHandsAnalyser/Program.cs
+++ PokerHandsAnalyser/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace PokerHandsAnalyser
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Poker game");
+
+            // read embedded resource -poker-hands.txt
+            var assembly = typeof(Program).GetTypeInfo().Assembly;
+            Stream resource = assembly.GetManifestResourceStream("PokerHandsAnalyser.InputFiles.poker-hands.txt");
+
+            using (StreamReader reader = new StreamReader(resource))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var game = line.Split(" ").ToList();
+                    var hand1 = game.Take(5);
+                    var hand2 = game.Skip(5);
+                    Console.WriteLine("\t" + "Player 1 hand: " + string.Join("", hand1) + " Player 2 hand: " + string.Join("", hand2));
+                }
+            }
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
         }
     }
 }
